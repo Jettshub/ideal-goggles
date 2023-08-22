bfb1b3ccf3b0ce836abb0759b410f13a091fc066
jettg15 <jetthuffhines@gmail.com>
2023-08-21 22:57:11 -0500

    added user prompts

--- Develop/script.js
+++ Develop/script.js
@@ -52,3 +52,31 @@ function generatePassword() {
 
 
 }
+
+//character length prompt
+function userPrompts() {
+    characterLength = parseInt(prompt("Choose the amount of characters for your password (8 - 128)"));
+    //is not a number prompt
+    if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+      alert("Please choose a number between 8 and 128 characters");
+      return false;
+    }
+    //asks for lower case prompt
+    if (confirm("Do you want to use lower case letters in your password?")) {
+      choiceArray = choiceArray.concat(lowerCase);
+    }
+    //asks for upper case prompt
+    if (confirm("Do you want to use upper case letters in your password?")) {
+      choiceArray = choiceArray.concat(upperCase);
+    }
+    //asks for numbers prompt
+    if (confirm("Do you want to use numbers letters in your password?")) {
+      choiceArray = choiceArray.concat(numericChar);
+    }
+    //asks for special character prompt
+    if (confirm("Do you want to use special characters in your password?")) {
+      choiceArray = choiceArray.concat(specialChar);
+    }
+    return true;
+
+  }
